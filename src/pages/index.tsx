9669bf461180abdc0ddfc4ca5113b9a865c80e5d
Gabriel <gabriel52.alves@gmail.com>
2023-02-07 15:44:53 -0300

    tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,26 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Layout from '@/components/Layout'
+import Tabela from '@/components/Tabela'
+import Cliente from '@/core/Cliente'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Gabriel', 'gabriel@gmail.com', 'Joinville SC', 18, '1'),
+    new Cliente('Juli', 'juli@gmail.com', 'Joinville SC', 37, '2'),
+    new Cliente('Vagner', 'vagner@gmail.com', 'Joinville SC', 37, '3'),
+  ]
+
   return (
     <div className={`
       flex justify-center items-center h-screen
       bg-fundo text-primary
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>  
+        <Tabela clientes={clientes}></Tabela> 
       </Layout>      
     </div>
   )
